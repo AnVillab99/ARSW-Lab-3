ff56e5a634fc7dd75912bfac1ff3ae48b7d82322
NigDra <andresvilla990223@gmail.com>
2019-01-31 10:03:43 -0500

    solo thread 1

---
+++ src/main/java/edu/eci/CountThreads/CountThreads.java
@@ -0,0 +1,15 @@
+package edu.eci.CountThreads;
+
+
+
+
+
+public class CountThreads{
+
+	public void main(String args[]) {
+		
+	}
+	
+	
+
+}
---
+++ src/main/java/edu/eci/CountThreads/counThreadsRun.java
@@ -0,0 +1,7 @@
+package edu.eci.CountThreads;
+
+public class counThreadsRun extends Thread{
+		
+	}
+
+
